391fc3e455c7b92d0b7a488f18bfab1b85d7c761
Amira <bcuba@unsa.edu.pe>
2025-10-29 15:22:55 -0500

    Ejercicio 4

--- script2.js
+++ script2.js
@@ -1,6 +1,7 @@
 let numeros = [1, 2, -3, 4, -5, 6, -7, 8, 9, -10];
 function filtrarYTransformar (arreglo) {
     let arregloTransformado = elevarAlCuadrado(eliminarNegativos(arreglo));
+    let sumaFinal = suma(arregloTransformado);
     function eliminarNegativos(arreglo){
         return arreglo.filter(item => item > 0);;
     }
@@ -8,6 +9,13 @@ function filtrarYTransformar (arreglo) {
         arreglo.map(item => item**2);
         return arreglo.map(item => item**2);;
     }
-    return arregloTransformado;
+    function suma (arreglo){
+        let suma = 0;
+        for (const item of arreglo){
+            suma += item;
+        }
+        return suma;
+    }
+    return sumaFinal;
 }
-console.log(`El doble del arreglo [${numeros}] es [${filtrarYTransformar(numeros)}] `);
+console.log(`El arreglo [${numeros}] filtrado y sumando sus elementos es ${filtrarYTransformar(numeros)} `);
